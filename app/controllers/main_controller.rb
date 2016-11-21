09a3c984ad0681c6dc6a3a1ae27d659ef9dfb3ec
AStasyK <kuchurina.a.a@gmail.com>
2016-11-21 18:27:28 +0000

    Lab 2

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,13 @@
+class MainController < ApplicationController
+  def index
+  end
+
+  def help
+  end
+
+  def about
+  end
+
+  def contacts
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  root 'main#index'
+
+  get 'main/index'
+
+  get 'main/help'
+
+  get 'main/about'
+
+  get 'main/contacts'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
