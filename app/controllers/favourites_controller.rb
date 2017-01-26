2c3926305ab87fdbbb90a8b066203c2a75ea53c4
Anastasia Kuchurina <AStasyK@users.noreply.github.com>
2017-01-26 11:52:13 +0300

    Favourite controller

---
+++ app/controllers/favourites_controller.rb
@@ -0,0 +1,31 @@
+class FavouritesController < ApplicationController
+  before_action :set_image, only: [:create]
+  before_action :set_favourite, only: [:destroy]
+  
+  def create
+    @favourite = Favourite.new(user_id: current_user.id)
+    @favourite.image_id = @image.id
+    respond_to do |format|
+      @favourite.save
+        format.js
+      else
+        format.html { render 'rate/index', alert: "Something's gone wrong" }
+      end
+    end
+  end
+
+  def destroy
+    @favourite.destroy
+    respond_to do |format|
+      format.js
+    end
+  end
+  
+  private
+  def set_image
+    @image = Image.find(params[:id])
+  end
+  def set_favourite
+    @favourite = Favourite.where(image_id: params[:id]).where(user_id: current_user.id)
+  end
+end
